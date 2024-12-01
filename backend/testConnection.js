fe52446c0e8e8f7fc22250995ab14c2b026cfbde
ZimraMohamed <zimramohamed64@gmail.com>
2024-12-01 12:50:04 +0530

    .gitignore done

--- backend/testConnection.js
+++
@@ -1,6 +0,0 @@
-mongoose.connect('mongodb://127.0.0.1:27017/softwaresecurity', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected successfully'))
-.catch((error) => console.log('MongoDB connection error:', error));
